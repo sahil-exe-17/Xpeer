43582fedba453965383eccb4ef2af0c4c83a3500
Sahil Lale <sahil@example.com>
2026-06-16 14:42:46 +0530

    Vercel SQLite hack

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,13 @@
-import app from '../server/src/app';
+import fs from 'fs';
+import path from 'path';
 
+// Vercel Serverless read-only filesystem hack
+const sourceDb = path.join(__dirname, '../server/prisma/dev.db');
+const targetDb = '/tmp/dev.db';
+
+if (!fs.existsSync(targetDb) && fs.existsSync(sourceDb)) {
+  fs.copyFileSync(sourceDb, targetDb);
+}
+
+import app from '../server/src/app';
 export default app;
